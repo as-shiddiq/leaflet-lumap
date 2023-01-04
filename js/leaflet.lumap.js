381493382ee9872349b96385d8d2328a92387e72
as-shiddiq <n.shiddiq@gmail.com>
2023-01-04 12:37:05 +0800

    tambah style

--- js/leaflet.lumap.js
+++ js/leaflet.lumap.js
@@ -8,7 +8,7 @@
     {
         let lm = this;
         let _html =``;
-        let _id;
+        let _idAside;
         let _elClick;
         let _layers = new Map();
         let _generated = false;
@@ -17,8 +17,8 @@
         lm.init = function()
         {
             let _i;
-            _id  = lm.makeId(5);
-            _html +=`<div class="accordion" id="lumap-${_id}">`;
+            _idAside  = `lumap-${lm.makeId(5)}`;
+            _html +=`<div class="accordion lumap-aside" id="${_idAside}">`;
             if(_el==null)
             {
                 console.error('Selector is null')
@@ -32,7 +32,7 @@
 
             _el.innerHTML = _html;
 
-            lm.onclick();
+            lm.onchange();
             _generated = true;
         }
 
@@ -44,15 +44,15 @@
             }
         }
 
-        lm.onclick = function()
+        lm.onchange = function()
         {
             if(!_generated)
             {
                 let _els = _el.querySelectorAll('.lm-click-layer');
                 for(_elClick of _els)
                 {
-                    _elClick.addEventListener('click',(e)=>{
-                        let _getId = e.target.getAttribute('for')??e.target.getAttribute('id');
+                    _elClick.addEventListener('change',(e)=>{
+                        let _getId = e.target.value;
                         let _chckd = document.querySelector(`input[id="${_getId}"]`).checked;
                         let _getLayer = _layers.get(_getId);
                         if(_getLayer.group.type=='single')
@@ -78,25 +78,71 @@
         lm.genBody = function(_d)
         {
             let _h = ``;
+            let _setId = `${_d.id}`;
+            let _setTitle = _d.title;
              _h += `<div class="accordion-item">
-                    <h2 class="accordion-header" id="headingOne">
-                      <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#header-${_id}${_d.id}" aria-expanded="true" aria-controls="collapseOne">
-                        ${_d.title}
-                      </button>
+                    <h2 class="accordion-header d-flex gap-2 align-items-center" id="headingOne">
+                        <input class="form-check-input lm-click-layer" type="checkbox" value="${_setId}">
+                      <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#header-${_setId}" aria-expanded="true" aria-controls="collapseOne">
+                        ${_setTitle}
+                     </button>
                     </h2>
-                    <div id="header-${_id}${_d.id}" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#lumap-${_id}">
+                    <div id="header-${_setId}" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#${_idAside}">
                       <div class="accordion-body">`;
-            if(_d.type=='single')
+            let _i;
+            for(_i in _d.child)
             {
-                _h += lm.genSingle(_d);
-            }
-            else{
-                _h += lm.genMultiple(_d);
+                let _setIdChild = `${_d.id}${_i}`
+                let _c = _d.child[_i];
+                lm.genLayers(_setIdChild,_c.layer,_d);
+                let _setType = 'checkbox';
+                if(_d.type=='single')
+                {
+                    _setType = 'radio';
+                }
+                _h += `<div class="form-check d-flex align-items-center justify-content-between gap-2">
+                            <div class="d-flex align-items-center gap-1">
+                              ${lm.setIcon(_c)}
+                              <label class="form-check-label" for="${_setIdChild}">
+                                ${_c.title}
+                              </label>
+                            </div>
+                          <input class="form-check-input lm-click-layer" name="${_setId}" type="${_setType}" value="${_setIdChild}" id="${_setIdChild}">
+                        </div>`;
+
             }
             _h += `</div>
                     </div>
                   </div>`;
             return _h;
+        }
+
+        lm.setStyle = function(_d) {
+            if(_d.style!=undefined)
+            {
+                return ` style="${_d.style}" `
+            }
+            return '';
+        }
+
+        lm.setIcon = function(_d) {
+            if(_d.icon!=undefined || _d.iconHtml!=undefined)
+            {
+                let _h = ``;
+                if(_d.iconHtml!=undefined)
+                {
+                    _h += `<div class="lm-icon lm-icon-image" ${lm.setStyle(_d)}>`;
+                    _h += _d.iconHtml;
+                }
+                else
+                {
+                    _h += `<div class="lm-icon" ${lm.setStyle(_d)}>`;
+                    _h += `<span class="bi bi-${_d.icon}"></span>`;
+                }
+                _h += `</div>`;
+                return _h;
+            }
+            return '';
         }
 
         lm.genLayers = function(_setId,_l,_d)
@@ -106,44 +152,6 @@
                 _layers.set(_setId,{layer:_l,group:_d});
             }
         }
-
-        lm.genSingle = function(_d)
-        {
-            let _h = ``;
-            let _i;
-            for(_i in _d.child)
-            {
-                let _setId = `${_d.id}${_i}`;
-                let _c = _d.child[_i];
-                lm.genLayers(_setId,_c.layer,_d);
-                _h += `<div class="form-check">
-                          <input class="form-check-input lm-click-layer" type="radio" name="flexRadioDefault" id="${_setId}">
-                          <label class="form-check-label" for="${_setId}">
-                            ${_c.title}
-                          </label>
-                        </div>`;
-            }
-            return _h;
-        };
-
-        lm.genMultiple = function(_d)
-        {
-            let _h = ``;
-            let _i;
-            for(_i in _d.child)
-            {
-                let _setId = `${_d.id}${_i}`;
-                let _c = _d.child[_i];
-                lm.genLayers(_setId,_c.layer,_d);
-                _h += `<div class="form-check">
-                          <input class="form-check-input lm-click-layer" type="checkbox" value="" id="${_setId}">
-                          <label class="form-check-label" for="${_setId}">
-                            ${_c.title}
-                          </label>
-                        </div>`;
-            }
-            return _h;
-        };
 
         lm.makeId = function (_length){
             var result           = '';
